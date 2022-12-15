4e136c0f6db6efc236d9f5f04be9d49474db13cb
Darlin Manuel Casado Pérez <70083547+Darlin12@users.noreply.github.com>
2022-12-15 10:52:39 -0400

    first commit

---
+++ response.py
@@ -0,0 +1,31 @@
+class miRespuestas:
+    res1 = 'No tengo edad, no soy un ser vivo'
+    resArray1 = ['edad', 'cuanto', 'tienes', 'años']
+
+    res2 = 'Lionel Messi'
+    resArray2 = ['futbol', 'quien', 'mundial', 'mejor']
+
+    res3 = 'Python'
+    resArray3 = ['lenguaje', 'programado', 'tecnologia', 'que']
+
+    res4 = 'No tengo padres, fui programado para ayudarte'
+    resArray4 = ['padres']
+
+    res5 = 'Nothing Phone 1'
+    resArray5 = [ 'telefono', 'favorito']
+
+    res6 = 'Luis Abinader'
+    resArray6 = ['presidente', 'republica dominicana', ]
+
+    res7 = 'Hay 5 continentes'
+    resArray7 = ['continentes', 'cuantos', 'hay']
+
+    res8 = 'ITLA'
+    resArray8 = ['universidad', 'estudio', 'que', 'en']
+
+    res9 = 'Darlin Manuel Casado Perez'
+    resArray9 = ['nombre', 'cual', 'mi']
+
+    res10 = 'Tienes 4 hermanos'
+    resArray10 = ['hermanos', 'cuantos', 'tengo']
+
